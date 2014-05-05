4e04dc73e78dd3e39a3b86759bc7cb3c02ca37ba
Ted Timmons <ted@perljam.net>
2014-05-04 17:17:51 -0700

    add reparsed/highlighted geojson

---
+++ highlight.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import json
+
+j = json.load(open('cop_fiber_hut_cand_sts_wgs84.geojson'))
+#print j
+for feature in j['features']:
+  v = feature['properties']['Viability']
+  if v == 'Not Viable':
+    feature['properties']['fill'] = '#f00'
+  elif v == 'Viable':
+    fc = feature['properties']['Fiber_Clas']
+    if fc == 'High Review':
+      feature['properties']['fill'] = '#0f0'
+    elif fc == 'Medium Review':
+      feature['properties']['fill'] = '#0b0'
+    elif fc == 'Low Review':
+      feature['properties']['fill'] = '#090'
+
+print json.dumps(j, indent=1)
