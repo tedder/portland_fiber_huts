9234ace2f230178563b1b0ecedc91948e37f3024
Ted Timmons <ted@perljam.net>
2014-05-04 17:29:46 -0700

    delete properly this time

--- highlight.py
+++ highlight.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 
 import json
+import sys
 
 j = json.load(open('cop_fiber_hut_cand_sts_wgs84.geojson'))
 #print j
+print >> sys.stderr, "starting with %g features" % len(j['features'])
+
 for n,feature in enumerate(j['features']):
   v = feature['properties']['Viability']
   if v == 'Not Viable':
-    feature['properties']['fill'] = '#f00 xx %s' % str(n)
-    del j['features'][n]
+    # we'll strip these later
+    pass
   elif v == 'Viable':
     fc = feature['properties']['Fiber_Clas']
     if fc == 'High Review':
@@ -18,4 +21,7 @@ for n,feature in enumerate(j['features']):
     elif fc == 'Low Review':
       feature['properties']['fill'] = '#090'
 
+j['features'] = [x for x in j['features'] if x['properties']['Viability'] == 'Viable']
+
+print >> sys.stderr, "finished with %g features" % len(j['features'])
 print json.dumps(j, indent=1)
